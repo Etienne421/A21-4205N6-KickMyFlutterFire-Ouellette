0b18eeeec4c994b9231c13bb55a8334859dd9243
Etienne <1849654@cegepmontpetit.ca>
2021-11-11 13:06:12 -0500

    Fin configuration Firebase

--- kick_my_flutter/lib/main.dart
+++ kick_my_flutter/lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:kick_my_flutter/accueil.dart';
@@ -8,6 +9,7 @@ import 'connection.dart';
 import 'i18n/intl_delegate.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -36,6 +38,48 @@ class MyApp extends StatelessWidget {
       home: ConnectionPage(title: 'Connection'),
       // home: InscriptionPage(title: 'Inscription',),
       //home: AccueilPage(title: 'Accueil',)
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Container(
+          height: 100,
+          width: 100,
+          color: Colors.green,
+        );
+      },
     );
   }
 }
